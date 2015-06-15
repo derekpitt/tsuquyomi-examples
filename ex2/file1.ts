ae5450a3b00d8a95419b21dee658448ab15c0c6f
Derek Pittsinger <derekpitt@gmail.com>
2015-06-15 09:41:23 -0500

    example

---
+++ ex2/file1.ts
@@ -0,0 +1,6 @@
+import { pow } from "file2"
+import { AppState } from "app-state"
+
+export function pow2() {
+  pow(new AppState());
+}
---
+++ ex2/file2.ts
@@ -0,0 +1,3 @@
+import { AppState } from "app-state"
+
+export function pow(a: AppState) { }
